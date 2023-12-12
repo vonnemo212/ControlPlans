85215481c7952fa34006d487ff115692eb50563f
Klemen Murn <klemen.murn1995@gmail.com>
2023-12-12 15:16:05 +0100

    Commit test

--- run-jar.bat
+++ run-jar.bat
@@ -1,3 +1,3 @@
 cd %~dp0\target
 pause
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:8000 .\ControlPlans-1.0-SNAPSHOT.jar
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=127.0.0.1:8001 .\ControlPlans-1.0-SNAPSHOT.jar
