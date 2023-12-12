387707405b9bd3f7d9565a31244467a2d5de4fba
Klemen Murn <klemen.murn1995@gmail.com>
2023-12-12 14:32:36 +0100

    Commit test

--- run-jar.bat
+++ run-jar.bat
@@ -1,2 +1,3 @@
-cd C:\Users\Administrator\IdeaProjects\ControlPlanes\target
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:8000 .\ControlPlanes-1.0-SNAPSHOT.jar
+cd %~dp0\target
+pause
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:8000 .\ControlPlans-1.0-SNAPSHOT.jar
